65cdfda1bf62007389dcfbbb4b7fe70bd8213020
djneeley <dan@neeleys.net>
2017-06-07 23:44:09 -0600

    add FVAnnuity

--- tvm.js
+++ tvm.js
@@ -111,6 +111,16 @@ function PVGrowingPerpetuity(A, i, g)
 	return A / (i - g);
 }
 
+function FVAnnuity(A, n, i)
+//returns the Future Value of an Annuity
+//A = Annuity Payment
+//n = Number of Periods
+//i = Interest Rate
+//https://en.wikipedia.org/wiki/Time_value_of_money#Future_value_of_an_annuity
+{
+	return A * (Math.pow(1+i, n) - 1) / i;
+}
+
 function FVGrowingAnnuity(A, n, i, g)
 //Returns the Future Value of a Growing Annuity
 //A = Annuity Payment
